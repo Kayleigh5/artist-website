c960192d6000afb88dcefeb9af29b142a1c9f22a
Kayleigh Beard <contact@kayleighbeard.nl>
2019-06-17 18:29:06 +0200

    Adjust loader, header, favicon

--- artist-website/src/app/components/home/home.component.ts
+++ artist-website/src/app/components/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   }
 
   dosomething() {
-    this.imageVisibility = 'unset';
+      this.imageVisibility = 'unset';
     this.loadingDisplay = 'none';
   }
 
