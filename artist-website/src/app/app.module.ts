17de5caa85d18e8f4694a9d015efe8ec4c2ef245
Kayleigh Beard <contact@kayleighbeard.nl>
2019-06-18 13:21:09 +0200

    Add menu & live page

--- artist-website/src/app/app.module.ts
+++ artist-website/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LiveComponent } from './components/live/live.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LiveComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
