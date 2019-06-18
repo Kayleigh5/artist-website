57a82532468c2d89ee736b751f57da5c346b8abc
Kayleigh Beard <contact@kayleighbeard.nl>
2019-06-18 17:13:12 +0200

    Finish Live page

--- artist-website/src/app/components/live/live.component.ts
+++ artist-website/src/app/components/live/live.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as globals from '../../globals/live-dates';
 
 @Component({
   selector: 'app-live',
@@ -9,7 +10,18 @@ export class LiveComponent implements OnInit {
 
   constructor() { }
 
+  liveDates: Array<Object>;
+
   ngOnInit() {
+    this.liveDates = globals.liveDates;
+  }
+
+  openLink(date) {
+    if (date.link) {
+      window.open(date.link, "_blank");
+    } else {
+      window.open("https://www.youtube.com/c/KayleighBeard5", "_blank")
+    }
   }
 
 }
---
+++ artist-website/src/app/globals/live-dates.ts
@@ -0,0 +1,153 @@
+// link is not 'required'
+
+export const liveDates: Array<Object> = [
+    {
+        date: '2019 Jan 17',
+        time: '20:30',
+        place: 'Electrosonic',
+        city: 'Groningen'
+    },
+    {
+        date: '2019 Feb 15',
+        time: '21:00',
+        place: 'Lola',
+        city: 'Groningen'
+    },
+    {
+        date: '2019 Mar 21',
+        time: '21:00',
+        place: 'SimplonUp',
+        city: 'Groningen'
+    },
+    {
+        date: '2019 Jan 12',
+        time: '23:00',
+        place: 'Studio Droom & Vreesmann',
+        city: 'Groningen'
+    },
+    {
+        date: '2018 Sep 21',
+        time: '19:30',
+        place: 'PhD Day Festival',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=G9GpW6mwruo'
+    },
+    {
+        date: '2018 June 23',
+        time: '20:00',
+        place: 'Headliners',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=g3Bf2AyJQKc'
+    },
+    {
+        date: '2018 May 19',
+        time: '12:00',
+        place: 'Frank Mohr',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=FNrMIexTflI'
+    },
+    {
+        date: '2018 May 13',
+        time: '14:30',
+        place: 'SIGN',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=ncpGTtvJXgU'
+    },
+    {
+        date: '2018 Apr 1',
+        time: '17:00',
+        place: 'Het Rumoer',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=koo1AWA86J8'
+    },
+    {
+        date: '2017 Dec 16',
+        time: '16:00',
+        place: 'Winterwelvaart',
+        city: 'Groningen'
+    },
+    {
+        date: '2017 Nov 25',
+        time: '16:00',
+        place: 'Upstream Gallery',
+        city: 'Amsterdam',
+        link: 'https://www.youtube.com/watch?v=QX6nLL4GnK4'
+    },
+    {
+        date: '2017 Nov 8',
+        time: '21:00',
+        place: 'Sounds of Music, Grand Theatre',
+        city: 'Groningen'
+    },
+    {
+        date: '2017 Oct 15',
+        time: '20:30',
+        place: 'Sunnyvale',
+        city: 'New York',
+        link: 'https://www.youtube.com/watch?v=LhrHwqvRF-w'
+    },
+    {
+        date: '2017 Sep 24',
+        time: '16:00',
+        place: 'DOT',
+        city: 'Groningen'
+    },
+    {
+        date: '2017 Jul 27',
+        time: '20:30',
+        place: 'Taribush Kuna',
+        city: 'Dwingeloo',
+        link: 'https://www.youtube.com/watch?v=R6yMeWVlj38'
+    },
+    {
+        date: '2017 May 20',
+        time: '15:00',
+        place: 'The World We Live In',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=k1pbYKIzq3k'
+    },
+    {
+        date: '2017 May 31',
+        time: '17:00',
+        place: 'De Nieuwe Kolk',
+        city: 'Assen',
+        link: 'https://www.youtube.com/watch?v=FOXu-aeN9ow'
+    },
+
+    {
+        date: '2017 Mar 20',
+        time: '17:30',
+        place: 'Frank Mohr',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=wZLv0lX44Ms'
+    },
+    {
+        date: '2017 Mar 08',
+        time: '12:00',
+        place: 'Minerva Theatre',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=oObt6i-4f-k'
+    },
+    {
+        date: '2017 Feb 06',
+        time: '18:00',
+        place: 'Club Transmediale',
+        city: 'Berlin'
+    },
+    {
+        date: '2016 Dec 10',
+        time: '21:00',
+        place: 'Freeze Festival',
+        city: 'Leeuwarden',
+        link: 'https://www.youtube.com/watch?v=nDIIYCHZU1E'
+    },
+    {
+        date: '2016 Nov 25',
+        time: '14:00',
+        place: 'Frank Mohr',
+        city: 'Groningen',
+        link: 'https://www.youtube.com/watch?v=PjfOdDUaVj8'
+    },
+
+    
+];
