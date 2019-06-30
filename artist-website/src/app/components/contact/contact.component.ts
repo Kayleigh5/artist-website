6ce9a1456237d41ed37bbd7af526b8c775ffea46
Kayleigh Beard <contact@kayleighbeard.nl>
2019-06-30 21:04:51 +0200

    Add social icons and image to contact page

--- artist-website/src/app/components/contact/contact.component.ts
+++ artist-website/src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-contact',
@@ -9,7 +10,35 @@ export class ContactComponent implements OnInit {
 
   constructor() { }
 
+  innerHtml: string;
+  link: string;
+  loading = true;
+
   ngOnInit() {
+    this.showContact();
+  }
+
+  showContact() {
+    setTimeout(() => {
+      this.make();
+    }, 5000);
+  }
+
+  make() {
+    const reverse = ['l', 'n', '.', 'd', 'r', 'a', 'e', 'b', 'h', 'g', 'i', 'e', 'l', 'y', 'a', 'k', '$', 't', 'c', 'a', 't', 'n', 'o', 'c'];
+    this.innerHtml =  this.replace(reverse).reverse().join('');
+    this.link = 'mailto:' + this.innerHtml;
+    this.loading = false;
+  }
+
+  replace (list: Array<string>): Array<string>  {
+    let newList = list;
+    for (let i = 0; i < list.length; i++) {
+      if(list[i] === '$') {
+        newList[i] = '@';
+      }
+    }
+    return newList;
   }
 
 }
