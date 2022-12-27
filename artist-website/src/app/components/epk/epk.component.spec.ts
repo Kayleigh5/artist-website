0184d4eb99dcb8c7a4a5bba612e268298df8609e
Kayleigh Beard <z753nnbujdmn@opayq.net>
2022-12-27 16:24:47 +0100

    Update to Angular 11

--- artist-website/src/app/components/epk/epk.component.spec.ts
+++ artist-website/src/app/components/epk/epk.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EpkComponent } from './epk.component';
 
@@ -6,7 +6,7 @@ describe('EpkComponent', () => {
   let component: EpkComponent;
   let fixture: ComponentFixture<EpkComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EpkComponent ]
     })
